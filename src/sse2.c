1ae7c961bbd560ce1cc0d78a123e552c0d90d188
Alecco <info@alejosanchez.com>
2011-04-02 20:30:22 -0300

    Improve comments

--- src/sse2.c
+++ src/sse2.c
@@ -14,10 +14,14 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+//  This file implements SSE2 primitives, mostly for sorting
+
+
 #include <stdint.h>
 #include <xmmintrin.h>
 
-typedef __v4si v4si; // For readability
+// A vector of 4 32bit signed integers (SSE2 128bit register)
+typedef __v4si v4si;
 
 // SSE2 lacks pmin/pmax for 32bit
 static void minmax_4si_sse2(v4si *a, v4si *b) {
@@ -38,6 +42,11 @@ static void column_sort_4si_sse2(v4si *a, v4si *b, v4si *c, v4si *d) {
 
 }
 
+//
+// Implementation of a bitonic merge on a 4x4 matrix
+//
+
+// Level 1 exchange
 static void bitonic_l1_exchange_4si_sse2(v4si *a, v4si *b) {
 
     __v4sf t0 = __builtin_ia32_movhlps ((__v4sf) *b,(__v4sf)  *a);
@@ -46,6 +55,7 @@ static void bitonic_l1_exchange_4si_sse2(v4si *a, v4si *b) {
     *a = (v4si) t1;
 }
 
+// Level 2 exchange
 static void bitonic_l2_exchange_4si_sse2(v4si *a, v4si *b) {
 
     __v4sf t0 = __builtin_ia32_unpckhps ((__v4sf) *a, (__v4sf) *b);
@@ -55,6 +65,7 @@ static void bitonic_l2_exchange_4si_sse2(v4si *a, v4si *b) {
 
 }
 
+// Level 3 exchange
 static void bitonic_l3_exchange_4si_sse2(v4si *a, v4si *b) {
 
     __v4sf t = __builtin_ia32_unpcklps ((__v4sf) *a, (__v4sf) *b);
@@ -63,6 +74,7 @@ static void bitonic_l3_exchange_4si_sse2(v4si *a, v4si *b) {
 
 }
 
+// Bitonic sort for 4 vectors of 4 32bit signed integers
 static void bitonic_sort_4si_sse2(v4si *a, v4si *b) {
 
     *a = __builtin_ia32_pshufd (*a, 0x1B); // Reverse a
@@ -76,7 +88,9 @@ static void bitonic_sort_4si_sse2(v4si *a, v4si *b) {
 
 }
 
-// Merge 2 pairs in parallel, same as bitonic(a,b) and bitonic(c,d)
+// Parallel bitonic sort for 2+2 vectors
+//   Same as bitonic(a,b) and bitonic(c,d)
+//   For latency hiding and better reciprocal throughput
 static void bitonic_sort_2x_4si_sse2(v4si *a, v4si *b, v4si *c, v4si *d) {
 
     *a = __builtin_ia32_pshufd (*a, 0x1B); // Reverse a
@@ -102,59 +116,56 @@ static void bitonic_sort_2x_4si_sse2(v4si *a, v4si *b, v4si *c, v4si *d) {
 
 }
 
+//
+// Bitonic full sort of 4 vectors of 4 32bit signed integers
+//   Sorts 16 elements
 static void sort_4x_4si_sse2(v4si *a, v4si *b, v4si *c, v4si *d) {
 
-    column_sort_4si_sse2(a, b, c, d);
+    column_sort_4si_sse2(a, b, c, d); // Get partial sort in registers
 
-    bitonic_sort_2x_4si_sse2(a, b, c, d);
+    bitonic_sort_2x_4si_sse2(a, b, c, d); // Sort (a,b) and (c,d)
 
     // Merge-exchange both heads (a and c)
-    bitonic_sort_4si_sse2(a, c);
-
-    // a is ready, has lowest
-
-    // Merge-exchange b and c
-    bitonic_sort_4si_sse2(b, c);
-    // b has 2nd lowest now
-
-    // Merge-exchange c and d
-    bitonic_sort_4si_sse2(c, d);
+    bitonic_sort_4si_sse2(a, c); // Merge sort (a,c)
+    // a has lowest 4 elements of original 16
+    bitonic_sort_4si_sse2(b, c); // Merge sort (b,c)
+    // b has 2nd lowest 4 elements
+    bitonic_sort_4si_sse2(c, d); // Merge sort (c,d)
+    // c has 3rd lowest 4 elements, d has the higher 4
 
 }
 
+//
+// Bitonic full sort of 8 vectors of 32bit signed integers
+//   Sorts 32 elements
+//   Intercalated sorting of the first 16 and second 16 (paths A, B)
 static void sort_8x_4si(v4si *a, v4si *b, v4si *c, v4si *d,
         v4si *e, v4si *f, v4si *g, v4si *h) {
 
-    column_sort_4si_sse2(a, b, c, d);
-    column_sort_4si_sse2(e, f, g, h);
+    column_sort_4si_sse2(a, b, c, d); // Column sort path A
+    column_sort_4si_sse2(e, f, g, h); // Column sort path B
 
-    bitonic_sort_2x_4si_sse2(a, b, c, d);
-    bitonic_sort_2x_4si_sse2(e, f, g, h);
+    bitonic_sort_2x_4si_sse2(a, b, c, d); // Sort path A
+    bitonic_sort_2x_4si_sse2(e, f, g, h); // Sort path B
 
-    // Merge-exchange both heads (a and c)
-    bitonic_sort_4si_sse2(a, c);
-    bitonic_sort_4si_sse2(e, g);
-
-    // a lowest of abcd
-    // e lowest of efgh
+    bitonic_sort_4si_sse2(a, c); // Exchange heads path A
+    bitonic_sort_4si_sse2(e, g); // Exchange heads path B
+    // a has 4 lowest of original (abcd), e lowest of original (efgh)
 
     // Merge-exchange b and c
-    bitonic_sort_4si_sse2(b, c);
-    bitonic_sort_4si_sse2(f, g);
-    // b has 2nd lowest now
+    bitonic_sort_4si_sse2(b, c); // Sort (b,c)
+    bitonic_sort_4si_sse2(f, g); // Sort (f,g)
+    // b and f have 2nd lowest of each path (A and B respectively)
 
-    // Merge-exchange c and d
-    bitonic_sort_4si_sse2(c, d);
-    bitonic_sort_4si_sse2(g, h);
+    bitonic_sort_4si_sse2(c, d); // Merge (c,d)
+    bitonic_sort_4si_sse2(g, h); // Merge (g,h)
+    // c and g have 3rd lowest of each path (A and B respectively)
 
-    // Both abcd and efgh sorted, merge both lists
-    // Note: imperfect sort, but very good on average
-    //       A perfect sort would involve a few conditionals
-    //       (Later merges will probably minimize this)
-
-    bitonic_sort_4si_sse2(a, e);
+    // Finally, merge paths A and B
+    bitonic_sort_4si_sse2(a, e); // Merge heads of paths A and B
     bitonic_sort_4si_sse2(b, f);
     bitonic_sort_4si_sse2(c, g);
     bitonic_sort_4si_sse2(d, h);
 
+    // XXX: Should check b < f and so on
 }
