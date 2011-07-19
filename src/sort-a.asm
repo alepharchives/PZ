3773688d4d431f1148e7c302e6f3174da08d5563
Alecco <info@alejosanchez.com>
2011-07-18 21:21:39 -0300

    Starting of SIMD version of sorting using x86asm
    Compiled with:
    yasm -f macho64 -m amd64 -DPIC -DPREFIX -DBIT_DEPTH=8 -DARCH_X86_64 -o sort-a.o sort-a.asm && gcc -Wall -o sort sort.c sort-a.o && ./sort

---
+++ src/sort-a.asm
@@ -0,0 +1,146 @@
+;;  Copyright (C) 2011  Alejo Sanchez www.ologan.com
+;;
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Affero General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU Affero General Public License for more details.
+;;
+;;  You should have received a copy of the GNU Affero General Public License
+;;  along with this program.  If not, see <http:;;www.gnu.org/licenses/>.
+
+%include "x86inc.asm"
+%include "x86util.asm"
+
+SECTION_RODATA
+
+; for reversing registers of 8 elements
+;;const pb_reverse,  db 7, 6, 5, 4, 3, 2, 1, 0
+
+SECTION .text
+
+; minmax + xor exchange, sse2 version
+;    mask = min(r1, r2)
+;    t0 = (r1 ^ r2) & mask
+;    r1 ^= t0   mins
+;    r2 ^= t0   maxs
+%macro MINMAXD 4 ; %1 reg1 %2 reg2 %3 mask %4 tmp
+    mova    m%3, m%1 ; copy for mask
+    mova    m%4, m%1 ; copy for tmp
+    pcmpgtd m%3, m%2 ; comp dw  %3 has mask
+    pxor    m%4, m%2 ; tmp ^= reg2
+    pand    m%4, m%3 ; tmp &= mask
+    pxor    m%1, m%4 ; reg1 ^= tmp  min
+    pxor    m%2, m%4 ; reg2 ^= tmp  max
+%endmacro
+
+%macro COLUMNSORT_4x4D 6 ; 2 reg 2 aux
+    MINMAXD %1, %3, %5, %6
+    MINMAXD %2, %4, %5, %6
+    MINMAXD %1, %2, %5, %6
+    MINMAXD %3, %4, %5, %6
+    MINMAXD %2, %3, %5, %6
+%endmacro
+
+; register sort 4 at a time
+%macro REGSORT      6 ; uses 6 xmm regs
+    COLUMNSORT_4x4D %1, %2, %3, %4, %5, %6 ; .6 cycles/elem
+    TRANSPOSE4x4D   %1, %2, %3, %4, %5     ; .3 cycles/elem
+%endmacro
+
+;       A4 A3 A2 A1
+;       B4 B3 B2 B1
+; reg1  A3 A2 A1 A0
+; reg2  B3 B2 B1 B0
+%macro BITONICL1D 3 ; %1 reg1, %2 reg2, %3 tmp0
+    movaps  m%3, m%2 ; save reg2 orig
+    movhlps m%2, m%1 ; B1B0A1A0 (reg2 reg1)
+    movlhps m%1, m%3 ; B3B2A3A2 (reg1 reg 2)
+%endmacro
+
+;       0 1 2 3
+;       4 5 6 7
+; L2P:  0 4 2 6   XXX this or B/A?
+; H2P:  1 5 3 7
+%macro BITONICL2D 4 ; %1 reg1, %2 reg2, %3 tmp1 %4 tmp2
+    mova      m%3, m%1 ; save copy reg1
+    unpcklps  m%1, m%2 ; t2 = unpcklps(r1,r2)
+    unpckhps  m%3, m%2 ; t1 = unpckhps(r1,r2)
+    mova      m%4, m%1 ; save copy reg1
+    mova      m%2, m%3 ; r2 = t1
+    movlhps   m%1, m%3 ; t2 = movlhps  (t2,t1)
+    movhlps   m%2, m%4 ; t1 = movhlps  (t2,t1)
+%endmacro
+
+;       0 1 2 3
+;       4 5 6 7
+; L3P:  0 4 1 5
+; H3P:  2 6 3 7
+%macro BITONICL3D 3 ; %1 reg1, %2 reg2, %3 tmp1
+    mova      m%3, m%1 ; t1 = r1
+    unpcklps  m%1, m%2 ; r1 = unpcklps(r1,r2)
+    unpckhps  m%3, m%2 ; t1 = unpcklps(t1,r2)
+    SWAP      m%3, m%2 ; r2 = t1
+%endmacro
+
+;%macro CRI 5 ; takes 2 add
+;    mova      m%2, [%1+ 0]       ; load first
+;    mova      m%3, [%1+16]       ; load first
+;    BITONICL3D  0, 1, 2
+;    mova  [%1+ 0], m%2
+;    mova  [%1+16], m%3
+;%endmacro
+
+%macro BITONIC_MERGED 4 ; takes 2 add
+    MINMAXD     %1, %2, %3, %4
+    BITONICL1D  %1, %2, %3
+    MINMAXD     %1, %2, %3, %4
+    BITONICL2D  %1, %2, %3, %4
+    MINMAXD     %1, %2, %3, %4
+    BITONICL3D  %1, %2, %3
+%endmacro
+
+; void cri(int n, int32_t *elem)
+cglobal cri, 2,2 ; n, src
+INIT_XMM
+    shr  r0, 4   ; Consume 2x4 elements at a time
+.bitonic_loop:
+    mova      m0, [r1+ 0]       ; load first
+    mova      m1, [r1+16]       ; load first
+    mova      m2, [r1+32]       ; load first
+    mova      m3, [r1+48]       ; load first
+    REGSORT 0, 1, 2, 3, 4, 5    ; 4 regs 2 aux
+    pshufd    m1, m1, 0x1b      ; reverse second
+    BITONIC_MERGED 0, 1, 4, 5   ; 2 regs 2 aux
+    pshufd    m3, m3, 0x1b     ; reverse fourth
+    BITONIC_MERGED 2, 3, 4, 5   ; 2 regs 2 aux
+    mova  [r1+ 0], m0
+    mova  [r1+16], m1
+    mova  [r1+32], m2
+    mova  [r1+48], m3
+    add        r1, 64  ; 2x4x4 bytes consumed
+    dec        r0
+    jg .bitonic_loop
+    RET
+
+%macro SORTW 1
+cglobal sort_%1, 3,4 ; use 1 more reg and less than 6 mmx/sse
+    ;; r0=n, r1=d[n], r2=aux[n]
+    sub     r2, r1   ; aux as offset of d
+    ;shr    r0d, 9   ; divide by 2*8 elems
+    shr    r0d, 4    ; // Consume 4x4 elements at a time (n/16*4B)
+.regsort_loop:
+    ;;prefetchnta [r1+256]
+    add    r1, 64 ; move forward 16 elements (4*mmsize)
+    dec    r0d    ; 4 regs done  (sets flag if 0)
+    jg     .regsort_loop
+    REP_RET       ; RET with Athlon friendly format
+%endmacro
+
+INIT_XMM
+SORTW ssse3
+
---
+++ src/sort.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "mm_malloc.h"
+
+void x264_sort_ssse3(int32_t n, int32_t *dst, int32_t *aux);
+void x264_cri(int n, int32_t *data);
+
+typedef void (sort_f)(int32_t, int32_t *, int32_t *);
+
+
+static inline uint64_t read_time(void)
+{
+    if(sizeof(long)==8)
+    {
+        uint64_t a, d;
+        asm volatile( "rdtsc\n\t" : "=a" (a), "=d" (d) );
+        return (d << 32) | (a & 0xffffffff);
+    } else {
+        uint64_t l;
+        asm volatile( "rdtsc\n\t" : "=A" (l) );
+        return l;
+    }
+}
+ 
+#define NOP_CYCLES 65 // time measured by an empty timer on Core2
+ 
+#define START_TIMER \
+uint64_t tend;\
+uint64_t tstart= read_time();
+
+#define STOP_TIMER(id) {\
+tend= read_time();\
+{\
+    static uint64_t tsum=0;\
+    static int tcount=0;\
+    static int tskip_count=0;\
+    if(tskip_count<2)\
+        tskip_count++;\
+    else{\
+    if(tcount<2 || tend - tstart < 8*tsum/tcount){\
+        tsum+= tend - tstart;\
+        tcount++;\
+    }else\
+        tskip_count++;\
+    if(((tcount+tskip_count) & (tcount+tskip_count-1)) == 0)\
+        printf("%"PRIu64" dezicycles in %s, %d runs, %d skips\n", tsum*10/tcount-NOP_CYCLES*10, id, tcount, tskip_count);\
+}}}
+
+#define STOP_TIMER_SUM(id) {\
+tend= read_time();\
+{\
+    static uint64_t tsum=0;\
+    static uint64_t tother=0;\
+    static uint64_t tend0=0;\
+    static int tcount=0;\
+    tsum += tend - tstart;\
+    if(tcount)\
+        tother += tstart - tend0;\
+    tend0 = tend;\
+    tcount++;\
+    if((tcount & (tcount-1)) == 0 && tcount > 4)\
+        printf("%"PRIu64"/%"PRIu64" cycles %s, %d runs\n", tsum-NOP_CYCLES*tcount, tother+tsum-NOP_CYCLES*tcount, id, tcount);\
+}}
+
+void sub(int n, sort_f f, char *name) {
+    int32_t  *d, *aux;
+    int i, tests;
+
+    d = _mm_malloc(n * sizeof (int32_t), 64);
+    aux = _mm_malloc(n * sizeof (int32_t), 64);
+
+    srandomdev(); // Init random pool
+    for (i = 0; i < n; i++)
+        d[i] = random() % 10;
+
+#ifdef DEBUG
+    for (i = 0; i < 32; i++)
+        printf("%i%c", d[i], (i+1) % 4? ' ' : '\n');
+    printf("--\n");
+#endif
+
+    for (tests = 4; tests; tests--) {
+        START_TIMER;
+        (*f)(n, d, aux);
+        STOP_TIMER("cri");
+    }
+
+#ifdef DEBUG
+    for (i = 0; i < 32; i++)
+        printf("%i%c", d[i], (i+1) % 4? ' ' : '\n');
+#endif
+
+#define TESTME 32
+    for (i = 0; i < TESTME; i++)
+        d[i] = random() % 10;
+    for (i = 0; i < TESTME; i++)
+        printf("%i%c", d[i], (i+1) % 4 ? ' ' : '\n');
+    printf("\n");
+    x264_cri(TESTME, d);
+    for (i = 0; i < TESTME; i++)
+        printf("%i%c", d[i], (i+1) % 4 ? ' ' : '\n');
+    _mm_free(d);
+    _mm_free(aux);
+}
+
+void b1(void) {
+    int32_t *d;
+    int i;
+
+    d = _mm_malloc(32, 16);
+    for (i = 0; i < 8; i++)
+        d[i] = i;
+
+    x264_cri(32, d);
+    for (i = 0; i < 8; i++)
+        printf("%i%c", d[i], (i+1) % 4 ? ' ' : '\n');
+}
+
+int main(int argc, char *argv[]) {
+
+    b1();
+    sub((1<<15), x264_sort_ssse3, "ssse3");
+
+    return (0);
+}
